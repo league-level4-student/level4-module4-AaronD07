004e5d49466e8743e181c46f15066c952d00ef62
AaronD07 <38016960+AaronD07@users.noreply.github.com>
2021-01-23 14:29:08 -0800

    :smile:

--- src/_03_polymorphs/BluePolymorph.java
+++ src/_03_polymorphs/BluePolymorph.java
@@ -12,7 +12,9 @@ public class BluePolymorph extends Polymorph{
 	@Override
 	public void draw(Graphics g) {
 		g.setColor(Color.blue);
-		g.fillRect(x, y, 50, 50);
+		g.fillRect(x, y, 50, 50); 
+		width = 0;
+		height =0;
 	}
 	
 }
