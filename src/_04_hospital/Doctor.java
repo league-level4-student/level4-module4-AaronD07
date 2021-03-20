e89a5f6d6c5855f5c20984831866ce7edef6525e
AaronD07 <38016960+AaronD07@users.noreply.github.com>
2021-03-20 11:28:21 -0700

    :smile:

--- src/_04_hospital/Doctor.java
+++ src/_04_hospital/Doctor.java
@@ -1,11 +1,12 @@
 package _04_hospital;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.print.Doc;
 
 public class Doctor {
-
+	ArrayList<Patient> patients = new ArrayList<Patient>();
 	public Object performsSurgery() {
 		// TODO Auto-generated method stub
 		return false;
@@ -15,20 +16,22 @@ public class Doctor {
 		// TODO Auto-generated method stub
 		return false;
 	}
-
-	public List<Doctor> getPatients() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 	public void assignPatient(Patient patient) {
 		// TODO Auto-generated method stub
 		
+		patients.add(patient);
 	}
+	public ArrayList<Patient> getPatients() {
+		// TODO Auto-generated method stub
+		return patients;
+	}
+
+
 
 	public void doMedicine() {
 		// TODO Auto-generated method stub
-
+		for(int i=0; i<patients.size(); i++) {
+patients.get(i).feelsCaredFor=true;
 	}
-
+	}
 }
